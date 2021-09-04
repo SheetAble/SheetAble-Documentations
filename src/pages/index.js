aaf8b23aaf31cab0ce977517ad2e306592cc0844
Valentin Zwerschke <v.zwerschke@mail.de>
2021-09-04 23:37:32 +0200

    Reset workflows

--- src/pages/index.js
+++ src/pages/index.js
@@ -19,7 +19,7 @@ function HomepageHeader() {
           <Link
             className="button button--secondary button--lg"
             to="/docs">
-            Get Startedededd - 5min ⏱️
+            Get Started - 5min ⏱️
           </Link>
         </div>
       </div>
